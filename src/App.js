b3c6b43b5ce3ce1fe5b039ccb73acb92269ed728
Volha Vetrava <volha.vetrava@gmail.com>
2018-10-24 13:03:44 +0300

    unnecessary comments deleted

--- src/App.js
+++ src/App.js
@@ -10,19 +10,6 @@ import reducer from './modules/mainPageFeed';
 const store = createStore(reducer, applyMiddleware(thunk));
 
 export default class App extends React.Component {
-    // componentDidMount() {
-    //     const config = {
-    //         apiKey: "AIzaSyB7GLZpv3hYESqIHAXleLL09gBcvJIxwo8",
-    //         authDomain: "ideas-app-d984b.firebaseapp.com",
-    //         databaseURL: "https://ideas-app-d984b.firebaseio.com",
-    //         projectId: "ideas-app-d984b",
-    //         storageBucket: "ideas-app-d984b.appspot.com",
-    //         messagingSenderId: "445764346787"
-    //     };
-    //     if (!firebase.apps.length) {
-    //         firebase.initializeApp(config);
-    //     }
-    // }
 
     render() {
         return (
